1cf699f4bbf92b4e903d8c5668bbda6d315fd818
nehasinghcode <78377676+nehasinghcode@users.noreply.github.com>
2025-07-15 16:57:35 +0530

    Create dynamodb.tf

---
+++ dynamodb.tf
@@ -0,0 +1,16 @@
+# Optional but recommended: DynamoDB table for state locking
+resource "aws_dynamodb_table" "tf_lock_table" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Lock Table"
+    Environment = "Dev"
+  }
+}
