d489d6ab04afb62ad4c1616784d465526a3c9976
nehasinghcode <78377676+nehasinghcode@users.noreply.github.com>
2025-07-15 16:19:46 +0530

    Create lambda.tf

---
+++ lambda.tf
@@ -0,0 +1,24 @@
+resource "aws_lambda_function" "trigger_glue_job" {
+  function_name = "trigger-glue-job-on-upload"
+  role          = aws_iam_role.lambda_glue_trigger_role.arn  # Defined in another file
+  handler       = "glue_trigger.lambda_handler"
+  runtime       = "python3.9"
+  timeout       = 30
+
+  filename         = "${path.module}/lambda/glue_trigger.zip"
+  source_code_hash = filebase64sha256("${path.module}/lambda/glue_trigger.zip")
+
+  environment {
+    variables = {
+      GLUE_JOB_NAME = "bank_csv_to_parquet_job"  # Replace with actual Glue job name
+    }
+  }
+}
+
+resource "aws_lambda_permission" "allow_s3_trigger" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.trigger_glue_job.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.raw_csv_bucket.arn
+}
