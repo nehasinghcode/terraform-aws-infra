fe1bb6b3307f90f4f8d2d25fd1c9fc2c17a21c2a
nehasinghcode <78377676+nehasinghcode@users.noreply.github.com>
2025-07-15 18:22:26 +0530

    Update iam_roles.tf

--- iam_roles.tf
+++ iam_roles.tf
@@ -41,3 +41,29 @@ resource "aws_iam_policy_attachment" "lambda_start_glue" {
   roles      = [aws_iam_role.lambda_glue_trigger_role.name]
   policy_arn = aws_iam_policy.glue_start_policy.arn
 }
+
+resource "aws_iam_role" "glue_job_role" {
+  name = "glue-job-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "glue.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_service_policy_attach" {
+  role       = aws_iam_role.glue_job_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full_access_attach" {
+  role       = aws_iam_role.glue_job_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
