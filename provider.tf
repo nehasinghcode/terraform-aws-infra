48f45dc94d78395d7f00c702e5d54c620e91eef3
nehasinghcode <78377676+nehasinghcode@users.noreply.github.com>
2025-07-15 16:59:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,18 +7,15 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
 
-terraform {
   backend "s3" {
-    bucket         = "finco-dev-tfstate-bucket"       # Create this S3 bucket manually
+    bucket         = "finco-dev-tfstate-bucket"       # Make sure this bucket exists
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"                 # Optional for locking
+    dynamodb_table = "terraform-lock"                 # Optional but recommended
   }
 }
-
 
 provider "aws" {
   region = "us-east-1"
