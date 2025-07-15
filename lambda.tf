e64afac64da243b1e5a705fe58f32844e88b7fbe
nehasinghcode <78377676+nehasinghcode@users.noreply.github.com>
2025-07-15 18:55:09 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,16 +1,16 @@
 resource "aws_lambda_function" "trigger_glue_job" {
   function_name = "trigger-glue-job-on-upload"
-  role          = aws_iam_role.lambda_glue_trigger_role.arn
   handler       = "glue_trigger.lambda_handler"
   runtime       = "python3.9"
-  timeout       = 30
-
-  filename         = "${path.module}/lambda_script/glue_trigger.zip"
+  role          = aws_iam_role.lambda_glue_trigger_role.arn
+  filename      = "${path.module}/lambda_script/glue_trigger.zip"
   source_code_hash = filebase64sha256("${path.module}/lambda_script/glue_trigger.zip")
+  timeout       = 30
+  memory_size   = 128
 
   environment {
     variables = {
-      GLUE_JOB_NAME = "bank_csv_to_parquet_job"
+      GLUE_JOB_NAME = aws_glue_job.bank_csv_to_parquet.name
     }
   }
 }
