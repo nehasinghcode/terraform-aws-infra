c8ac6aaa8c5c46f40404719c7f3a24bbae9a9763
nehasinghcode <78377676+nehasinghcode@users.noreply.github.com>
2025-07-15 18:21:30 +0530

    Update glue_job.tf

--- glue_job.tf
+++ glue_job.tf
@@ -1,27 +1,16 @@
-resource "aws_glue_job" "bank_csv_to_parquet_job" {
+resource "aws_glue_job" "bank_csv_to_parquet" {
   name     = "bank_csv_to_parquet_job"
-  role_arn = aws_iam_role.glue_service_role.arn
-  glue_version      = "4.0"
-  max_capacity      = 2
-  number_of_workers = 2
-  worker_type       = "G.1X"
-  timeout           = 10
+  role_arn = aws_iam_role.glue_job_role.arn
 
   command {
     name            = "glueetl"
-    script_location = "s3://${aws_s3_bucket.curated_csv_bucket.bucket}/scripts/bank_csv_to_parquet.py"
+    script_location = "s3://finco-dev-glue-scripts-us-east-1-001/scripts/bank_csv_to_parquet.py"
     python_version  = "3"
   }
 
-  default_arguments = {
-    "--job-language"        = "python"
-    "--TempDir"             = "s3://${aws_s3_bucket.curated_csv_bucket.bucket}/temp/"
-    "--enable-metrics"      = "true"
-    "--enable-continuous-cloudwatch-log" = "true"
-  }
-
-  tags = {
-    "Name"        = "BankCSVToParquetJob"
-    "Environment" = "Dev"
-  }
+  max_retries      = 0
+  timeout          = 10
+  glue_version     = "4.0"
+  number_of_workers = 2
+  worker_type       = "G.1X"
 }
