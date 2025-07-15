e44043f934846ab8c8f3f992118190894086e18b
nehasinghcode <78377676+nehasinghcode@users.noreply.github.com>
2025-07-15 13:47:39 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
