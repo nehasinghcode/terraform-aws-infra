68d2c61aedce12637c9a321b886bcbc2ce8d1e16
nehasinghcode <78377676+nehasinghcode@users.noreply.github.com>
2025-07-15 21:48:16 +0530

    Update glue_catalogue.tf

--- glue_catalogue.tf
+++ glue_catalogue.tf
@@ -97,3 +97,109 @@ resource "aws_glue_catalog_table" "bank_data_parquet" {
     type = "string"
   }
 }
+
+
+resource "aws_glue_catalog_table" "bank_raw_table" {
+  name          = "bank_raw_data"
+  database_name = "bank_marketing_curated"
+
+  table_type = "EXTERNAL_TABLE"
+
+  storage_descriptor {
+    location      = "s3://finco-dev-raw-csv-data-us-east-1-001/"
+    input_format  = "org.apache.hadoop.mapred.TextInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat"
+
+    ser_de_info {
+      name                  = "OpenCSVSerde"
+      serialization_library = "org.apache.hadoop.hive.serde2.OpenCSVSerde"
+
+      parameters = {
+        "separatorChar" = ";"
+        "quoteChar"     = "\""
+        "escapeChar"    = "\\"
+      }
+    }
+
+    columns {
+      name = "age"
+      type = "int"
+    }
+    columns {
+      name = "job"
+      type = "string"
+    }
+    columns {
+      name = "marital"
+      type = "string"
+    }
+    columns {
+      name = "education"
+      type = "string"
+    }
+    columns {
+      name = "default"
+      type = "string"
+    }
+    columns {
+      name = "balance"
+      type = "int"
+    }
+    columns {
+      name = "housing"
+      type = "string"
+    }
+    columns {
+      name = "loan"
+      type = "string"
+    }
+    columns {
+      name = "contact"
+      type = "string"
+    }
+    columns {
+      name = "day"
+      type = "int"
+    }
+    columns {
+      name = "month"
+      type = "string"
+    }
+    columns {
+      name = "duration"
+      type = "int"
+    }
+    columns {
+      name = "campaign"
+      type = "int"
+    }
+    columns {
+      name = "pdays"
+      type = "int"
+    }
+    columns {
+      name = "previous"
+      type = "int"
+    }
+    columns {
+      name = "poutcome"
+      type = "string"
+    }
+    columns {
+      name = "y"
+      type = "string"
+    }
+
+    compressed        = false
+    stored_as_sub_directories = false
+    skip_header_line_count    = 1
+  }
+
+  parameters = {
+    "classification"        = "csv"
+    "skip.header.line.count" = "1"
+    "compressionType"        = "none"
+    "typeOfData"             = "file"
+  }
+}
+
